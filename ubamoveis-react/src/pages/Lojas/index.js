f1cf6a3cd1a2e2b5e963820feeea35a27e373962
Junior Cintra <apspvcintraj@gmail.com>
2021-04-09 11:54:46 -0300

    deletando comentarios e oficializando mudanças

--- ubamoveis-react/src/pages/Lojas/index.js
+++ ubamoveis-react/src/pages/Lojas/index.js
@@ -1,11 +1,4 @@
 import React, { useState } from "react";
-
-import {
-  GoogleMap,
-  LoadScript,
-  Marker,
-  InfoWindow,
-} from "@react-google-maps/api";
 import Select from "react-select";
 import { FaWhatsapp as WhatsApp } from "react-icons/fa";
 
@@ -27,17 +20,13 @@ import {
   NomeLoja,
   Content,
   QrCodeZap,
-  Iframe
+  Iframe,
 } from "./styles";
 
 import LojasImg from "../../assets/images/lojas_interna.png";
 import qrVoltaRedonda from "../../assets/images/qr_voltaredonda.jpeg";
 import qrBarraDoPirai from "../../assets/images/qr_barradopirai.jpeg";
 import qrBarraMansa from "../../assets/images/qr_barramansa.jpeg";
-
-
-
-
 
 export default function Lojas() {
   const cidadeOptions = [
@@ -57,45 +46,26 @@ export default function Lojas() {
 
   const [cidade, setCidade] = useState(cidadeOptions[0].value);
   const [cidadeLabel, setCidadeLabel] = useState(cidadeOptions[0].label);
-  const [cidadeLocation, setCidadeLocation] = useState(
-    cidadeOptions[0].location
-  );
-  const [selected, setSelected] = useState({});
 
   const { innerWidth: width } = window;
-
-  const onSelect = (item) => {
-    setSelected(item);
-  };
-
-  const mapStyles = {
-    height:
-      width < 980 && width > 660 ? "300px" : width < 660 ? "250px" : "500px",
-    width:
-      width < 980 && width > 660 ? "500px" : width < 660 ? "300px" : "800px",
-  }; 
 
   function handleCidade(value) {
     switch (value.label) {
       case "Volta Redonda":
         setCidade("(24) 99317-9372");
         setCidadeLabel("Volta Redonda");
-        setCidadeLocation(value.location);
         break;
       case "Barra Mansa":
         setCidade("(24) 99318-4972");
         setCidadeLabel("Barra Mansa");
-        setCidadeLocation(value.location);
         break;
       case "Barra do Piraí":
         setCidade("(24) 99317-8979");
         setCidadeLabel("Barra do Piraí");
-        setCidadeLocation(value.location);
         break;
       default:
         setCidade("(24) 99317-9372");
         setCidadeLabel("Volta Redonda");
-        setCidadeLocation(value.location);
         break;
     }
   }
@@ -160,61 +130,63 @@ export default function Lojas() {
             />
           </Unidades>
 
-
           <div>
-          {cidadeLabel === "Volta Redonda" ? (
-                <Iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1842.8756359613847!2d-44.09525609186238!3d-22.513513418506143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9ea2a74fd24105%3A0x61ca8bbbde02ba80!2zVWLDoSBNw7N2ZWlz!5e0!3m2!1spt-BR!2sbr!4v1617928610772!5m2!1spt-BR!2sbr"
-
-                  height={width < 980 && width > 660 ? "300px" : width < 660 ? "250px" : "500px"}
-
-                  width={ width < 980 && width > 660 ? "500px" : width < 660 ? "300px" : "800px"}
-                    />
+            {cidadeLabel === "Volta Redonda" ? (
+              <Iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1842.8756359613847!2d-44.09525609186238!3d-22.513513418506143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9ea2a74fd24105%3A0x61ca8bbbde02ba80!2zVWLDoSBNw7N2ZWlz!5e0!3m2!1spt-BR!2sbr!4v1617928610772!5m2!1spt-BR!2sbr"
+                height={
+                  width < 980 && width > 660
+                    ? "300px"
+                    : width < 660
+                    ? "250px"
+                    : "500px"
+                }
+                width={
+                  width < 980 && width > 660
+                    ? "500px"
+                    : width < 660
+                    ? "300px"
+                    : "800px"
+                }
+              />
             ) : cidadeLabel === "Barra Mansa" ? (
-                <Iframe 
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58959.28811518679!2d-44.14880692028493!3d-22.543339398161653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9e989318f30e29%3A0xa122f56b2d0c267d!2zVWLDoSBNw7N2ZWlzIEV4dHJh!5e0!3m2!1spt-BR!2sbr!4v1617929051472!5m2!1spt-BR!2sbr" 
-                    
-                  height={width < 980 && width > 660 ? "300px" : width < 660 ? "250px" : "500px"}
-
-                  width={ width < 980 && width > 660 ? "500px" : width < 660 ? "300px" : "800px"}
-                />
+              <Iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58959.28811518679!2d-44.14880692028493!3d-22.543339398161653!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9e989318f30e29%3A0xa122f56b2d0c267d!2zVWLDoSBNw7N2ZWlzIEV4dHJh!5e0!3m2!1spt-BR!2sbr!4v1617929051472!5m2!1spt-BR!2sbr"
+                height={
+                  width < 980 && width > 660
+                    ? "300px"
+                    : width < 660
+                    ? "250px"
+                    : "500px"
+                }
+                width={
+                  width < 980 && width > 660
+                    ? "500px"
+                    : width < 660
+                    ? "300px"
+                    : "800px"
+                }
+              />
             ) : (
-              <Iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.110823817711!2d-43.827588484894456!3d-22.462469327814638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9eb50bcee26519%3A0x54d89b5203b39455!2sUba%20M%C3%B3veis!5e0!3m2!1spt-BR!2sbr!4v1617929501256!5m2!1spt-BR!2sbr" 
-
-              height={width < 980 && width > 660 ? "300px" : width < 660 ? "250px" : "500px"}
-
-              width={ width < 980 && width > 660 ? "500px" : width < 660 ? "300px" : "800px"}
-          />
+              <Iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.110823817711!2d-43.827588484894456!3d-22.462469327814638!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9eb50bcee26519%3A0x54d89b5203b39455!2sUba%20M%C3%B3veis!5e0!3m2!1spt-BR!2sbr!4v1617929501256!5m2!1spt-BR!2sbr"
+                height={
+                  width < 980 && width > 660
+                    ? "300px"
+                    : width < 660
+                    ? "250px"
+                    : "500px"
+                }
+                width={
+                  width < 980 && width > 660
+                    ? "500px"
+                    : width < 660
+                    ? "300px"
+                    : "800px"
+                }
+              />
             )}
           </div>
-          
-          {/* <LoadScript googleMapsApiKey="AIzaSyB3iYXBKTf27MACNe7AJrthMse6Ug544gA">
-            <GoogleMap
-              className="mapa"
-              mapContainerStyle={mapStyles}
-              zoom={16}
-              center={cidadeLocation}>
-              {cidadeOptions.map((item) => {
-                return (
-                  <Marker
-                    key={item.label}
-                    position={item.location}
-                    onClick={() => onSelect(item)}
-                    label="Ubá Móveis"
-                  />
-                );
-              })}
-              {selected.location && (
-                <InfoWindow
-                  position={selected.location}
-                  clickable={true}
-                  onCloseClick={() => setSelected({})}>
-                  <p>{selected.name}</p>
-                </InfoWindow>
-              )}
-            </GoogleMap>
-          </LoadScript> */}
         </Content>
       </DivUnidades>
       <Newsletter />
@@ -222,5 +194,4 @@ export default function Lojas() {
       <Footer />
     </>
   );
-
-              }
+}
